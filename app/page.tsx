4052ca0fdf4580461f33d89ae0e4dac9ee26adc8
eid iepoh <tornike.dzmanashvili@gmail.com>
2023-06-16 18:54:12 +0400

    added constants and testing implementations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import Image from 'next/image'
-
-const img = "/gurl.webp";
+import Image from 'next/image';
+import { intergrations } from '@/constants/index';
 
 export default function Home() {
   return (
     <div className="poka">
-
+      {intergrations.map(item => (
+        <div>
+          <h1>{item.name}</h1>
+          <h2>{item.description}</h2>
+          <Image
+            src={item.icon}
+            width={48}
+            height={48}
+            alt={`${item.name} icon`}
+          />
+        </div>
+      ))}
     </div>
   )
 }
---
+++ constants/index.ts
@@ -0,0 +1,34 @@
+export const businessPlan = ["Small Business", "Medium Business", "Enterprise"];
+
+export const intergrations = [
+    {
+        name: "Sapling",
+        description: "HR Management",
+        icon: "/saplingLogo.svg"
+    },
+    {
+        name: "Workday",
+        description: "HR Management",
+        icon: "/workdayLogo.svg"
+    },
+    {
+        name: "Xero",
+        description: "Employers Base",
+        icon: "/xeroLogo.svg"
+    },
+    {
+        name: "Rippling",
+        description: "HR Management",
+        icon: "/ripplingLogo.svg"
+    },
+    {
+        name: "Expensify",
+        description: "HR Management",
+        icon: "/expensifyLogo.svg"
+    },
+    {
+        name: "Zenefits",
+        description: "HR Management",
+        icon: "/zenefitsLogo.svg"
+    },
+]
